248130f660e2a42709ee53b1658fb80abdb15ad8
Danny Gurman <danny.gurman@algosec.com>
2020-11-24 15:05:49 +0200

    Spring mvc 7

--- spring-examples/spring-mock-mvc/src/test/java/mockmvcexample/AbstractControllerTest.java
+++ spring-examples/spring-mock-mvc/src/test/java/mockmvcexample/AbstractControllerTest.java
@@ -2,11 +2,23 @@ package mockmvcexample;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import mockmvcexample.config.MyServiceTestConfig;
+import mockmvcexample.controlllers.exceptionhandler.MyServiceExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.BeforeClass;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.mockito.Mockito.mock;
 
 //At its core, the TestContext framework allows you to annotate test classes with @ContextConfiguration to specify which configuration
 //files to use to load the ApplicationContext for your test.
@@ -20,4 +32,28 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 @WithUserDetails(userDetailsServiceBeanName = "testUserDetailsService")
 public class AbstractControllerTest {
 
+    private final static Authentication authentication = mock(Authentication.class);
+
+    private  HttpMessageConverter[] messageConverters =
+        {new MappingJackson2HttpMessageConverter(), new ByteArrayHttpMessageConverter() };
+
+    @Value("${v1API}")
+    protected String v1API;
+
+    protected MockMvc mvc;
+
+
+
+    @BeforeClass
+    public static void mockSpringSecurity() {
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    protected void setUp(Object... controllers) {
+        mvc = standaloneSetup(controllers)
+            .addPlaceholderValue("v1API", v1API)
+            .setMessageConverters(messageConverters)
+            .setControllerAdvice(new MyServiceExceptionHandler())
+            .build();
+    }
 }
