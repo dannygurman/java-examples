1b697d7290a2aa64c2bc4953fae79b16bd266996
danny <danny.gurman@gmail.com>
2020-12-22 23:41:18 +0200

    Semaphore 2

---
+++ java-examples-generic/src/main/java/concurrency/semaphore/ex2/DelayQueueUsingTimedSemaphore.java
@@ -0,0 +1,22 @@
+package concurrency.semaphore.ex2;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.concurrent.TimedSemaphore;
+
+class DelayQueueUsingTimedSemaphore {
+
+    private final TimedSemaphore semaphore;
+
+    DelayQueueUsingTimedSemaphore(long period, int slotLimit) {
+        semaphore = new TimedSemaphore(period, TimeUnit.SECONDS, slotLimit);
+    }
+
+    boolean tryAdd() {
+        return semaphore.tryAcquire();
+    }
+
+    int availableSlots() {
+        return semaphore.getAvailablePermits();
+    }
+}
