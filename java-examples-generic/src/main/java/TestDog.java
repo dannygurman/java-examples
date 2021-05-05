fb736ca732663ed25eb4ed187180d0a05b888e5e
Danny Gurman <danny.gurman@algosec.com>
2021-05-05 17:32:05 +0300

    Java basic5

---
+++ java-examples-generic/src/main/java/TestDog.java
@@ -0,0 +1,15 @@
+public class TestDog {
+
+    public static void main(String[] args) {
+        Dog brakakoko = new Dog();
+        Dog shimon_peres = new Dog();
+
+        brakakoko.name = "brakako";
+     //   shimon_peres.name = "david ben gorion";
+
+        brakakoko.bark();
+       // shimon_peres.bark();
+
+    }
+
+}
