809b05ba52220611b4c6b2a419f3db402acb6505
dannygurman <danny.gurman@gmail.com>
2021-05-31 20:35:16 +0300

    BST4

--- algorithms-examples/src/main/java/algorithms/tree/binary/BinaryTreeUtils.java
+++ algorithms-examples/src/main/java/algorithms/tree/binary/BinaryTreeUtils.java
@@ -1,5 +1,7 @@
 package algorithms.tree.binary;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.function.BiFunction;
 
 //Utils for regular (NOT BST) binary tree
@@ -11,17 +13,17 @@ public class BinaryTreeUtils {
     // Root of Binary Tree
     public Node root;
 
-    BinaryTreeUtils()    {
+    BinaryTreeUtils() {
         root = null;
     }
 
 
     // see algorithms-examples\src\main\java\algorithms\tree\binary\example_binary_tree.gif
-    public static BinaryTreeUtils buildExampleRegularBinaryTree(){
+    public static BinaryTreeUtils buildExampleRegularBinaryTree() {
 
-         //         1
-       //     2            3
-     //  4          5    6      7
+        //         1
+        //     2            3
+        //  4          5    6      7
 
         BinaryTreeUtils tree = new BinaryTreeUtils();
         tree.root = new Node(MIN_VALUE);
@@ -36,7 +38,7 @@ public class BinaryTreeUtils {
     }
 
 
-    //----------------------------------------
+    //---------------MIN MAX -------------------------
 
 
     public static int getRegularBinaryTreeMinElement(Node root) {
@@ -49,31 +51,31 @@ public class BinaryTreeUtils {
         return getMinOrMaxElement(compareFunction, root);
     }
 
-    private static int getMinOrMaxElement(BiFunction<Integer, Integer, Integer> compareFunction, Node root){
+    private static int getMinOrMaxElement(BiFunction<Integer, Integer, Integer> compareFunction, Node root) {
         //Check whether tree is empty
-        if(root == null) {
+        if (root == null) {
             System.out.println("Tree is empty");
             return 0;
-        }  else {
+        } else {
             return getMinOrMaxElementInternal(compareFunction, root);
         }
     }
 
-    private static int getMinOrMaxElementInternal(BiFunction<Integer, Integer, Integer> compareFunction ,Node node){
+    private static int getMinOrMaxElementInternal(BiFunction<Integer, Integer, Integer> compareFunction, Node node) {
 
         int leftMaxOrMin, rightMaxOrMin;
         //Max will store temp's data
         int maxOrMin = node.value;
 
         //It will find largest element in left subtree
-        if(node.left != null){
+        if (node.left != null) {
             leftMaxOrMin = getMinOrMaxElementInternal(compareFunction, node.left);
             //Compare max with leftMax and store greater value into max
             maxOrMin = compareFunction.apply(maxOrMin, leftMaxOrMin);
         }
 
         //It will find largest element in right subtree
-        if(node.right != null){
+        if (node.right != null) {
             rightMaxOrMin = getMinOrMaxElementInternal(compareFunction, node.right);
             //Compare max with rightMax and store greater value into max
             maxOrMin = compareFunction.apply(maxOrMin, rightMaxOrMin);
@@ -81,7 +83,111 @@ public class BinaryTreeUtils {
         return maxOrMin;
     }
 
-   //------------------------------------------
+    //------------------------------------------
 
+
+    //------ DELETE
+    // Function to delete deepest element in binary tree
+    //Using queue instead of recursion
+  // Assumption - the node  to delete is already the DEEPESET (last leaf)
+
+   private static void deleteDeepest(Node root, Node nodeToDelete) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        Node temp;
+        // Do level order traversal until last node (adding right the left)
+        while (!q.isEmpty()) {
+            temp = q.peek();
+            q.remove();
+
+            if (temp == nodeToDelete) {
+                //we found it  -delete by setting null;
+                temp = null;
+                return;
+            }
+
+            //searching on right
+            if (temp.right != null) {
+                if (temp.right == nodeToDelete) {
+                    //we found it  -delete by setting null;
+                    temp.right = null;
+                    return;
+                } else
+                    q.add(temp.right);
+            }
+
+            if (temp.left != null) {
+                if (temp.left == nodeToDelete) {
+                    //we found it  -delete by setting null;
+                    temp.left = null;
+                    return;
+                } else
+                    q.add(temp.left);
+            }
+        }
+
+    }
+
+
+
+
+
+
+/*    Function to delete given element in binary tree
+      Algorithm :
+    1. Starting at root, find the deepest and RIGHT MOSET node in binary tree and node which we want to delete.
+    2. Replace the deepest rightmost node’s data with node to be deleted.
+    3. Then delete the deepest rightmost node.
+
+    See deletion-in-binary-tree.png
+
+    //Note: We can also replace node’s data that is to be deleted with any node whose left and right child
+     points to NULL but we only use deepest node in order to maintain the BALANCE34  of a binary tree.
+
+    */
+    static void deleteNodeByValue(Node root, int nodeToDeleteKey)
+    {
+        if (root == null)
+            return;
+
+        if (root.left == null &&
+                root.right == null)
+        {
+            if (root.value == nodeToDeleteKey)
+            {
+                root=null;
+                return;
+            }
+            else
+                return;
+        }
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+        Node temp = null, keyNode = null;
+
+        // Do level order traversal until
+        // we find key and last node.
+        while (!q.isEmpty())
+        {
+            temp = q.peek();
+            q.remove();
+
+            if (temp.value == nodeToDeleteKey)
+                keyNode = temp;
+
+            if (temp.left != null)
+                q.add(temp.left);
+
+            if (temp.right != null)
+                q.add(temp.right);
+        }
+
+        if (keyNode != null)
+        {
+            int x = temp.value;
+            deleteDeepest(root, temp);
+            keyNode.value = x;
+        }
+    }
 
 }
