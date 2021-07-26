6644ce592649eba85efdd05f22bb49c9fecdf729
Danny Gurman <danny.gurman@algosec.com>
2021-07-26 14:56:51 +0300

    maxpath2

--- algorithms-examples/src/main/java/algorithms/arrays/matrixUniquePaths/MatrixUniquePathsRecursion.java
+++ algorithms-examples/src/main/java/algorithms/arrays/matrixUniquePaths/MatrixUniquePathsRecursion.java
@@ -9,18 +9,21 @@ public class MatrixUniquePathsRecursion {
     }
 
     // Returns count of possible paths to reach
-    // cell at row number m and column number n
-    // from the topmost leftmost cell (cell at 1, 1)
-    static int  numberOfPaths(int m, int n)
+    // cell at row number 'row' and column number 'column'
+    // from the topmost-leftmost cell (cell at 1, 1)
+    //you can either move only to right or down
+    static int  numberOfPaths(int row, int column)
     {
         // If either given row number is first or
         // given column number is first
-        if (m == 1 || n == 1)
+        //There is one way - down till end  OR right till end
+        if (row == 1 || column == 1) {
             return 1;
+        }
 
         // If diagonal movements are allowed then  the last addition is required.
-        return  numberOfPaths(m-1, n) + numberOfPaths(m, n-1);
-        // + numberOfPaths(m-1,n-1);
+        return  numberOfPaths(row-1, column) + numberOfPaths(row, column-1);
+        // + numberOfPaths(row-1,column-1);
     }
 
 }
