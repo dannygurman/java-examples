cdd01a9e5aad06f198c6df7ef9fa580c4deb01df
Danny Gurman <danny.gurman@algosec.com>
2021-08-05 19:37:24 +0300

    compare example continue2

---
+++ java-examples-generic/src/main/java/compareSort/arraySort/SortStringArrayTest.java
@@ -0,0 +1,37 @@
+package compareSort.arraySort;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class SortStringArrayTest {
+
+    String[] fruits;
+    String[] expectedSortedFruitsByName;
+    String[] expectedSortedFruitsByReverseNameOrder;
+
+    @Before
+    public void before() {
+        String pine = "Pineapple" , apple = "Apple", orange = "Orange", banana = "Banana";
+        fruits = new String[] {pine, apple , orange, banana};
+        expectedSortedFruitsByName = new String[] {apple, banana, orange, pine};
+    }
+
+    @Test
+    public void testSortStringArray() {
+        Arrays.sort(fruits);
+
+        iterateArrayAndPrint(fruits);
+        Assert.assertArrayEquals( expectedSortedFruitsByName, fruits);
+    }
+
+
+    private void iterateArrayAndPrint(String[] fruits){
+        int i=0;
+        for(String fruit: fruits){
+            System.out.println("fruits " + ++i + " : " + fruit);
+        }
+    }
+}
